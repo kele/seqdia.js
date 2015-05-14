af456685f232e1c9af443ce12cca27c0491c8b40
kele <damian.bogel@gmail.com>
2015-05-14 16:40:45 +0200

    Major update

--- seqdia.js
+++ seqdia.js
@@ -42,18 +42,33 @@ var Drawing = {
     {
         var group = draw.group().stroke({width : 2});
 
-        var line = draw.line(x1, y, x2, y);
-        group.add(line);
-
-        if (direction == 'rtl')
+        if (x1 == x2) 
         {
-            group.add(draw.line(x1, y, x1 + 10, y - 2));
-            group.add(draw.line(x1, y, x1 + 10, y + 2));
-        }
-        else if (direction == 'ltr')
-        {
+            var line = draw.polyline([[x1, y + 5],
+                                  [x1 + 20, y + 5],
+                                  [x1 + 20, y + 10],
+                                  [x1 - 20, y + 10],
+                                  [x1 - 20, y],
+                                  [x1, y]]).stroke({width : 2}).fill('none');;
+            group.add(line);
             group.add(draw.line(x2, y, x2 - 10, y - 2));
             group.add(draw.line(x2, y, x2 - 10, y + 2));
+        }
+        else
+        {
+            var line = draw.line(x1, y, x2, y);
+            group.add(line);
+
+            if (direction == 'rtl')
+            {
+                group.add(draw.line(x1, y, x1 + 10, y - 2));
+                group.add(draw.line(x1, y, x1 + 10, y + 2));
+            }
+            else if (direction == 'ltr')
+            {
+                group.add(draw.line(x2, y, x2 - 10, y - 2));
+                group.add(draw.line(x2, y, x2 - 10, y + 2));
+            }
         }
 
         return group;
@@ -62,7 +77,8 @@ var Drawing = {
 
 function Actor(name, colour, draw, height)
 {
-    this.name = name
+    this.name = name;
+    this.colour = colour;
 
     this.topDrawing = Drawing.createTextBox(name, colour, draw);
     this.topDrawing.center(0, 0);
@@ -71,6 +87,7 @@ function Actor(name, colour, draw, height)
     this.bottomDrawing.center(0, this.topDrawing.rbox().cy + this.topDrawing.rbox().height + height);
 
     this.lifeline = Drawing.createLifeline(this.topDrawing, this.bottomDrawing, colour, draw);
+    this.lifeline.node.title = name;
     this.lifeline.node.onclick = function(event) { console.log(name, event); };
 
     this.drawing = draw.group();
@@ -84,12 +101,12 @@ function Actor(name, colour, draw, height)
     this.moveTo = function(x, y) { this.drawing.move(x, y); }
 }
 
-function Message(from, to, label, content)
+function Message(id, from, to, label)
 {
+    this.id = id;
     this.from = from;
     this.to = to;
     this.label = label;
-    this.content = content;
 }
 
 function Diagram()
@@ -107,9 +124,9 @@ function Diagram()
         this.actorsNames.add(name);
     }
 
-    this.addMessage = function(from, to, label, content)
+    this.addMessage = function(id, from, to, label)
     {
-        this.messages.push(new Message(from, to, label, content));
+        this.messages.push(new Message(id, from, to, label));
     }
 
     this.clear = function()
@@ -120,7 +137,8 @@ function Diagram()
 
     this.getHeight = function()
     {
-        return MESSAGE_HEIGHT*this.messages.length + 2*ACTORBOX_HEIGHT; // TODO: WIP
+        return MESSAGE_HEIGHT * this.messages.length
+            + 2 * ACTORBOX_HEIGHT; // TODO: WIP
     }
 
     this.getWidth = function()
@@ -152,11 +170,6 @@ function Diagram()
         return array;
     }
 
-    function getRandomColor()
-    {
-        return Please.make_color();
-    }
-
     function getPositions(arr)
     {
         var pos = {};
@@ -168,8 +181,9 @@ function Diagram()
     function computeGaps (positions, messages, labels)
     {
         var gaps = [];
-        for (i = 0; i < messages.length; i++)
-            gaps.push(0);
+        for (i = 0; i < Object.keys(positions).length; i++)
+            gaps.push(100);
+
         for (i = 0; i < messages.length; i++)
         {
             var m = messages[i];
@@ -198,10 +212,11 @@ function Diagram()
     {
         var actors = [];
         var start = 100;
+        var colors = Please.make_color({colors_returned: names.length});
         for (i = 0; i < names.length; i++)
         {
             var name = names[i];
-            actors.push(new Actor(name, getRandomColor(), draw, height));
+            actors.push(new Actor(name, colors[i], draw, height));
             actors[i].moveTo(start, TOP_MARGIN);
 
             start += gaps[i];
@@ -211,17 +226,27 @@ function Diagram()
 
     function createMessagePopup(msg)
     {
-        return msg.content;
+        return msg;
     }
 
     function drawLegend(actors)
     {
-        // TODO
+        var legendText = "";
+        for (var a of actors)
+        {
+            legendText += "<span style=\"color: " + a.colour + ";\"><strong>" + a.name + "</strong></span><br/>\n";
+        }
+
+        $("div#legend").html(legendText).show();
     }
     
     function displayPopup(x, y, popup)
     {
-        console.log(x, y, popup);
+        $("div#msg_" + popup.id)
+            .toggle()
+            .css("position", "absolute")
+            .css("top", y + "px")
+            .css("left", x + "px");
     }
 
     this.createDrawing = function()
@@ -231,8 +256,9 @@ function Diagram()
         var actorPositions = getPositions(names);
         var messagesLabels = createMessageLabels(this.messages, draw);
         var gaps = computeGaps(actorPositions, this.messages, messagesLabels);
-
         var actors = createActors(names, gaps, this.getHeight(), draw);
+        
+        // TODO: align actors if they overlap
 
         for (i = 0; i < this.messages.length; i++)
         {
@@ -244,17 +270,35 @@ function Diagram()
             var y = (i + 2) * 50;
 
             messagesLabels[i].move(actors[left].drawing.rbox().cx + MESSAGE_BOTH_SIDES_MARGIN/2, y);
-            messagesLabels[i].node.popup = createMessagePopup(this.messages[i]);
-            messagesLabels[i].node.onclick = function(event) { displayPopup(event.pageX, event.pageY, this.popup); };
+            messagesLabels[i].node.popup_info = createMessagePopup(this.messages[i]);
+            messagesLabels[i].node.onclick = function(event) { displayPopup(event.pageX, event.pageY, this.popup_info); };
 
             Drawing.createMessageLine(actors[left].drawing.rbox().cx, actors[right].drawing.rbox().cx, messagesLabels[i].rbox().y2 + 2, direction, draw);
         }
 
+        setTimeout(function() { drawLegend(actors); }, 200);
         drawLegend(actors);
 
         var widht = 2500; // WIP
-        draw.size(widht, this.getHeight() + 200);
+        draw.size(widht, this.getHeight() + 100);
+
 
         return draw;
     }
 }
+
+function prepareUi() {
+    $("div.popup_callstack_button").click(function() {
+        $(this).siblings(".popup_callstack").toggle();
+    });
+
+    $("div.popup").draggable({ handle: "div.handle", stack: "div.popup"});
+
+    $("div.handle").dblclick(function() {
+        $(this).parent().toggle();
+    });
+
+    $("div#legend").hover(function() { $(this).css("opacity", 0.2); },
+                          function() { $(this).css("opacity", 1); });
+}
+setTimeout(prepareUi, 200);
