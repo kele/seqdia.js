c9190f45e2ab86be950579913b92a162ceafc0ed
kele <damian.bogel@gmail.com>
2015-05-14 11:28:59 +0200

    Working stuff:  - colours  - messages with arrows (no content, though)  - lifeline height

--- seqdia.js
+++ seqdia.js
@@ -1,11 +1,11 @@
 var Drawing = {
-    createTextBox: function(content, draw)
+    createTextBox: function(content, colour, draw)
     {
         var text = draw.text(content);
 
-        var box = draw.rect(text.rbox().width + 15, text.rbox().height + 15);
-        box.fill('rgba(255, 255, 255, 0)');
-        box.attr({ 'stroke-width' : '1px'});
+        var box = draw.rect(text.rbox().width + 15, text.rbox().height + 15)
+            .fill(colour)
+            .attr({ 'stroke-width' : '1px'});
 
         text.center(box.rbox().cx, box.rbox().cy);
 
@@ -16,57 +16,66 @@ var Drawing = {
         return group;
     },
 
-    createLifeline: function(topBox, bottomBox, draw)
+    createLifeline: function(topBox, bottomBox, colour, draw)
     {
         var topBB = topBox.rbox();
         var bottomBB = bottomBox.rbox();
 
         console.assert(topBB.cx == bottomBB.cx, "x-center of actor boxes are not equal");
 
-        var line = draw.line(topBB.cx, topBB.y2, bottomBB.cx, bottomBB.y).stroke({ width: 1 });
+        var line = draw.line(topBB.cx, topBB.y2, bottomBB.cx, bottomBB.y).stroke({ width: 5, color: colour });
         return line;
     },
 
     createMessageLabel: function(label, draw)
     {
-        var text = draw.text(label);
+        var text = draw.text(label).font({
+            'family' : 'Monospace'
+        });
+
+        text.node.onclick = function() { console.log("You clicked :)"); }; // TODO
+
         return text;
     },
 
-    // TODO: refactor
-    createMessageLine: function(x1, x2, y, draw)
+    createMessageLine: function(x1, x2, y, direction, draw)
     {
-        var line = draw.line(x1, y, x2, y).stroke({ width: 1 });
+        var group = draw.group().stroke({width : 2});
 
-        // TODO: add arrow
-
-        var group = draw.group();
+        var line = draw.line(x1, y, x2, y);
         group.add(line);
+
+        if (direction == 'rtl')
+        {
+            group.add(draw.line(x1, y, x1 + 10, y - 2));
+            group.add(draw.line(x1, y, x1 + 10, y + 2));
+        }
+        else if (direction == 'ltr')
+        {
+            group.add(draw.line(x2, y, x2 - 10, y - 2));
+            group.add(draw.line(x2, y, x2 - 10, y + 2));
+        }
 
         return group;
     },
-
-    createMessageDrawing: function(label, line)
-    {
-        // TODO
-    }
 };
 
-function Actor(name, draw, height)
+function Actor(name, colour, draw, height)
 {
     this.name = name
 
-    this.topDrawning = Drawing.createTextBox(name, draw);
-    this.topDrawning.center(0, 0);
+    this.topDrawing = Drawing.createTextBox(name, colour, draw);
+    this.topDrawing.center(0, 0);
 
-    this.bottomDrawning = Drawing.createTextBox(name, draw);
-    this.bottomDrawning.center(0, this.topDrawning.rbox().cy + this.topDrawning.rbox().height + 500);
+    this.bottomDrawing = Drawing.createTextBox(name, colour, draw);
+    this.bottomDrawing.center(0, this.topDrawing.rbox().cy + this.topDrawing.rbox().height + height);
 
-    this.lifeline = Drawing.createLifeline(this.topDrawning, this.bottomDrawning, draw);
+    this.lifeline = Drawing.createLifeline(this.topDrawing, this.bottomDrawing, colour, draw);
+    this.lifeline.node.onclick = function(event) { console.log(name, event); };
 
     this.drawing = draw.group();
-    this.drawing.add(this.topDrawning);
-    this.drawing.add(this.bottomDrawning);
+    this.drawing.add(this.topDrawing);
+    this.drawing.add(this.bottomDrawing);
     this.drawing.add(this.lifeline);
 
     this.rbox = function() { return this.drawing.rbox(); }
@@ -83,17 +92,11 @@ function Message(from, to, label, content)
     this.content = content;
 }
 
-function MessageDrawing(msg, y, x1, x2, direction)
-{
-    // TODO
-}
-
-
 function Diagram()
 {
-    var MESSAGE_HEIGHT = 100;
+    var MESSAGE_HEIGHT = 50;
     var TOP_MARGIN = 50;
-    var ACTORBOX_HEIGHT = 100; // TODO: cut it out
+    var ACTORBOX_HEIGHT = 50; // TODO: cut it out
     var MESSAGE_BOTH_SIDES_MARGIN = 20;
 
     this.actorsNames = new Set();
@@ -142,15 +145,19 @@ function Diagram()
         return messagesBySender;
     }
 
-    var setToArray = function(set)
+    function setToArray(set)
     {
         var array = [];
         for (var k of set) array.push(k);
         return array;
-
     }
 
-    var getPositions = function(arr)
+    function getRandomColor()
+    {
+        return Please.make_color();
+    }
+
+    function getPositions(arr)
     {
         var pos = {};
         for (i = 0; i < arr.length; i++)
@@ -158,54 +165,90 @@ function Diagram()
         return pos;
     }
 
-    this.createDrawing = function()
+    function computeGaps (positions, messages, labels)
     {
-        var draw = SVG('canvas').size(500, 500); // WIP
-        var actors = [];
-        var names = setToArray(this.actorsNames);
-        var actorPositions = getPositions(names);
-
         var gaps = [];
-        for (i = 0; i < this.messages.length; i++)
+        for (i = 0; i < messages.length; i++)
             gaps.push(0);
+        for (i = 0; i < messages.length; i++)
+        {
+            var m = messages[i];
+            var labelWidth = labels[i].rbox().width;
+            
+            var left = Math.min(positions[m.from], positions[m.to]);
+            gaps[left] = Math.max(gaps[left], labelWidth + MESSAGE_BOTH_SIDES_MARGIN);
+        }
+        return gaps;
+    }
 
+    function createMessageLabels (messages, draw)
+    {
         var messagesLabels = [];
         var messageByLeftAnchor = {};
-        for (i = 0; i < this.messages.length; i++)
+        for (i = 0; i < messages.length; i++)
         {
-            var m = this.messages[i];
-            var labelDrawing = Drawing.createMessageLabel(m.label, draw);
+            var m = messages[i];
+            var labelDrawing = Drawing.createMessageLabel((i + 1) + ". " + m.label, draw);
             messagesLabels.push(labelDrawing);
-            
-            var left = Math.min(actorPositions[m.from], actorPositions[m.to]);
-            gaps[left] = Math.max(gaps[left], labelDrawing.rbox().width + MESSAGE_BOTH_SIDES_MARGIN);
         }
+        return messagesLabels;
+    }
 
-        console.log(gaps);
-
+    function createActors(names, gaps, height, draw)
+    {
+        var actors = [];
         var start = 100;
         for (i = 0; i < names.length; i++)
         {
             var name = names[i];
-            actors.push(new Actor(name, draw, this.getHeight()));
-            console.log(start);
+            actors.push(new Actor(name, getRandomColor(), draw, height));
             actors[i].moveTo(start, TOP_MARGIN);
 
             start += gaps[i];
         }
+        return actors;
+    }
+
+    function createMessagePopup(msg)
+    {
+        return msg.content;
+    }
+
+    function drawLegend(actors)
+    {
+        // TODO
+    }
+
+    this.createDrawing = function()
+    {
+        var draw = SVG('canvas').size(500, 500); // WIP
+        var names = setToArray(this.actorsNames);
+        var actorPositions = getPositions(names);
+        var messagesLabels = createMessageLabels(this.messages, draw);
+        var gaps = computeGaps(actorPositions, this.messages, messagesLabels);
+
+        var actors = createActors(names, gaps, this.getHeight(), draw);
 
         for (i = 0; i < this.messages.length; i++)
         {
             var m = this.messages[i];
             var left = Math.min(actorPositions[m.from], actorPositions[m.to]);
             var right = Math.max(actorPositions[m.from], actorPositions[m.to]);
+            var direction = left == actorPositions[m.from] ? 'ltr' : 'rtl';
 
-            var y = (i + 1) * 100;
+            var y = (i + 2) * 50;
+
             messagesLabels[i].move(actors[left].drawing.rbox().cx + MESSAGE_BOTH_SIDES_MARGIN/2, y);
-            Drawing.createMessageLine(actors[left].drawing.rbox().cx, actors[right].drawing.rbox().cx, messagesLabels[i].rbox().y2 + 2, draw);
+            messagesLabels[i].node.popup = createMessagePopup(this.messages[i]);
+            messagesLabels[i].node.onclick = function() { console.log(this.popup); };
+
+            Drawing.createMessageLine(actors[left].drawing.rbox().cx, actors[right].drawing.rbox().cx, messagesLabels[i].rbox().y2 + 2, direction, draw);
         }
 
-        draw.size(start + 100, this.getHeight());
+        drawLegend(actors);
+
+        var widht = 2500; // WIP
+        draw.size(widht, this.getHeight() + 200);
 
         return draw;
     }
